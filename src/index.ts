07deefe8f0c648a230ad3a1a0d7a6625e4e73b64
Kai Sassnowski <me@kai-sassnowski.com>
2024-01-29 05:41:50 +0100

    fix: export component types properly

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
-import Angle from "@/components/Angle.vue";
-import Arc from "@/components/Arc.vue";
-import Circle from "@/components/Circle.vue";
-import Ellipse from "@/components/Ellipse.vue";
-import FunctionPlot from "@/components/FunctionPlot.vue";
-import Graph from "@/components/Graph.vue";
-import Label from "@/components/Label.vue";
-import Line from "@/components/Line.vue";
-import Point from "@/components/Point.vue";
-import Polygon from "@/components/Polygon.vue";
-import PolyLine from "@/components/PolyLine.vue";
-import Sector from "@/components/Sector.vue";
-import Vector from "@/components/Vector.vue";
-import { Vector2 } from "@/math/Vector2";
-import { useColors } from "@/composables/useColors";
-
-export {
-  Angle,
-  Arc,
-  Circle,
-  Ellipse,
-  FunctionPlot,
-  Graph,
-  Label,
-  Line,
-  Point,
-  Polygon,
-  PolyLine,
-  Sector,
-  Vector,
-  Vector2,
-  useColors,
-};
+export { default as Angle } from "@/components/Angle.vue";
+export { default as Arc } from "@/components/Arc.vue";
+export { default as Circle } from "@/components/Circle.vue";
+export { default as Ellipse } from "@/components/Ellipse.vue";
+export { default as FunctionPlot } from "@/components/FunctionPlot.vue";
+export { default as Graph } from "@/components/Graph.vue";
+export { default as Label } from "@/components/Label.vue";
+export { default as Line } from "@/components/Line.vue";
+export { default as Point } from "@/components/Point.vue";
+export { default as Polygon } from "@/components/Polygon.vue";
+export { default as PolyLine } from "@/components/PolyLine.vue";
+export { default as Sector } from "@/components/Sector.vue";
+export { default as Vector } from "@/components/Vector.vue";
+export { Vector2 } from "@/math/Vector2";
+export { useColors } from "@/composables/useColors";
