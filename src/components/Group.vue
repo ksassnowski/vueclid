bdb27c3ec0e96d9eef2a3d347bf9038bbc88f6f4
Kai Sassnowski <me@kai-sassnowski.com>
2024-08-21 12:46:24 +0200

    feat: add group component

---
+++ src/components/Group.vue
@@ -0,0 +1,16 @@
+<template>
+  <g v-bind="$attrs">
+    <slot />
+  </g>
+</template>
+
+<script lang="ts" setup>
+import { toRef } from "vue";
+import { PossibleVector2, Vector2 } from "../utils/Vector2.ts";
+import { useLocalToWorld } from "../composables/useLocalToWorld.ts";
+
+const props = withDefaults(
+  defineProps<{ position?: PossibleVector2; }>(), { position: () => new Vector2() });
+
+useLocalToWorld(toRef(props, "position"));
+</script>
